dda2f431c7e9c9478e149f15fb46744aa0f94efa
banana1324 <57201624+banana1324@users.noreply.github.com>
2026-09-04 16:58:53 -0700

    Add: add-two-numbers - submission-0

---
+++ Data Structures & Algorithms/add-two-numbers/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        curr = head
+        carry = False
+
+        while l1 or l2:
+            dig1 = 0
+            dig2 = 0
+
+            if l1:
+                dig1 = l1.val
+            if l2:
+                dig2 = l2.val
+
+            total = dig1 + dig2
+
+            if carry:
+                total += 1
+
+            curr.val = total % 10
+            carry = total >= 10
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+            if l1 or l2:
+                curr.next = ListNode()
+                curr = curr.next
+
+        if carry:
+            curr.next = ListNode(1)
+
+        return head
